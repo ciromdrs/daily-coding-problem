b20cd88da8507f2a280de74951c6fbdc2b3e04db
Ciro <ciromoraismedeiros@gmail.com>
2018-11-27 14:33:34 -0300

    Problem 36

---
+++ dcp36_second_largest.py
@@ -0,0 +1,8 @@
+'''Given the root to a binary search tree, find the second largest node
+in the tree.
+'''
+
+'''
+Solution: go right until it reaches a leaf, then go left once and go
+right again until it reaches a leaf (this is the second largest node).
+'''
