22446b634995cbed7bf36c5f0d5800f5cf2346bb
Ciro <ciromoraismedeiros@gmail.com>
2018-12-07 14:24:35 -0300

    Problem 44

---
+++ dcp44_array_inversions.py
@@ -0,0 +1,45 @@
+'''We can determine how "out of order" an array A is by counting the
+number of inversions it has. Two elements A[i] and A[j] form an
+inversion if A[i] > A[j] but i < j. That is, a smaller element appears
+after a larger element.
+
+Given an array, count the number of inversions it has. Do this faster
+than O(N^2) time.
+
+You may assume each element in the array is distinct.
+
+For example, a sorted list has zero inversions. The array
+[2, 4, 1, 3, 5] has three inversions: (2, 1), (4, 1), and (4, 3).
+The array [5, 4, 3, 2, 1] has ten inversions: every distinct pair forms
+an inversion.'''
+
+'''Solution: implement insertion-sort and, at every element swap, print
+the pair of elements swapped.
+
+Example:
+A is the original array, B is the sorted array.
+
+has three inversions: (2, 1), (4, 1), and (4, 3).
+
+Iteration 1
+A = [2, 4, 1, 3, 5] 
+B = [2,           ]
+
+Iteration 2
+A = [2, 4, 1, 3, 5] 
+B = [2, 4,        ]
+
+Iteration 3 
+A = [2, 4, 1, 3, 5] 
+B = [1, 2, 4,     ]
+Print (2,1) and (4,1)
+
+Iteration 4
+A = [2, 4, 1, 3, 5] 
+B = [1, 2, 3, 4,  ]
+Print (4,3)
+
+Iteration 5
+A = [2, 4, 1, 3, 5] 
+B = [1, 2, 3, 4, 5]
+'''
